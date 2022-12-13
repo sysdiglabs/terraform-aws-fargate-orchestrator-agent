4526eecdf42fc91e8726aea41ef16c07967924bc
Lars Eric Scheidler <lars-eric.scheidler@ratepay.com>
2022-12-13 09:49:10 +0100

    allow usage of a secret key for access key, terraform fmt

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,29 +1,29 @@
 resource "aws_lb" "orchestrator_agent" {
-  internal = true
+  internal           = true
   load_balancer_type = "network"
-  ip_address_type = "ipv4"
-  subnets = var.subnets
+  ip_address_type    = "ipv4"
+  subnets            = var.subnets
 
   tags = merge(var.tags, var.default_tags)
 }
 
 resource "aws_lb_target_group" "orchestrator_agent" {
-  port = var.orchestrator_port
-  protocol = "TCP"
-  target_type = "ip"
+  port                 = var.orchestrator_port
+  protocol             = "TCP"
+  target_type          = "ip"
   deregistration_delay = 60
-  vpc_id = var.vpc_id
+  vpc_id               = var.vpc_id
 
   tags = merge(var.tags, var.default_tags)
 }
 
 resource "aws_lb_listener" "orchestrator_agent" {
   load_balancer_arn = aws_lb.orchestrator_agent.arn
-  port = var.orchestrator_port
-  protocol = "TCP"
+  port              = var.orchestrator_port
+  protocol          = "TCP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.orchestrator_agent.arn
   }
 
--- variables.tf
+++ variables.tf
@@ -3,23 +3,35 @@
 #
 variable "name" {
   description = "Identifier for module resources"
-  type = string
-  default = "sysdig-fargate-orchestrator"
+  type        = string
+  default     = "sysdig-fargate-orchestrator"
 }
 
 variable "vpc_id" {
   description = "ID of the VPC where the orchestrator should be installed"
-  type = string
-}
-
-variable "access_key" {
-  description = "Sysdig access key"
-  type = string
+  type        = string
 }
 
 variable "subnets" {
   description = "A list of subnets that can access the internet and are reachable by instrumented services. The subnets must be in at least 2 different AZs."
-  type = list(string)
+  type        = list(string)
+}
+
+variable "access_key" {
+  description = "Sysdig access key. Either var.access_key or var.access_key_secret_name and var.access_key_secret_arn must be set."
+  type        = string
+}
+
+variable "access_key_secret_key_name" {
+  description = "The key name in Sysdig access key secret. Either var.access_key or var.access_key_secret_name and var.access_key_secret_arn must be set. If var.access_key_secret_key_name is null and var.access_key_secret_arn is set, the secret value itself is used."
+  type        = string
+  default     = null
+}
+
+variable "access_key_secret_arn" {
+  description = "Sysdig access key secret arn. Either var.access_key or var.access_key_secret_name and var.access_key_secret_arn must be set."
+  type        = string
+  default     = null
 }
 
 #
@@ -27,57 +39,57 @@ variable "subnets" {
 #
 variable "orchestrator_port" {
   description = "Port for the workload agent to connect"
-  type = number
-  default = 6667
+  type        = number
+  default     = 6667
 }
 
 variable "agent_image" {
   description = "Orchestrator agent image"
-  type = string
-  default = "quay.io/sysdig/orchestrator-agent:latest"
+  type        = string
+  default     = "quay.io/sysdig/orchestrator-agent:latest"
 }
 
 variable "collector_host" {
   description = "Sysdig collector host"
-  type = string
-  default = "collector.sysdigcloud.com"
+  type        = string
+  default     = "collector.sysdigcloud.com"
 }
 
 variable "collector_port" {
   description = "Sysdig collector port"
-  type = string
-  default = "6443"
+  type        = string
+  default     = "6443"
 }
 
 variable "agent_tags" {
   description = "Comma separated list of tags for this agent"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "check_collector_certificate" {
   description = "Whether to check the collector certificate when connecting. Mainly for development."
-  type = string
-  default = "true"
+  type        = string
+  default     = "true"
 }
 
 variable "assign_public_ip" {
   description = "Provisions a public IP for the service. Required when using an Internet Gateway for egress."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "tags" {
   description = "Extra tags for all Sysdig Fargate Orchestrator resources"
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 variable "default_tags" {
   description = "Default tags for all Sysdig Fargate Orchestrator resources"
-  type = map(string)
+  type        = map(string)
   default = {
     Application = "sysdig"
-    Module = "fargate-orchestrator-agent"
+    Module      = "fargate-orchestrator-agent"
   }
 }
