70cf8254c8cfefc2a9848252477fc5fb187a64c6
francesco-racciatti <francesco.racciatti@sysdig.com>
2022-12-19 12:11:53 +0100

    feat: support secretsmanager-backed sysdig access key
    Signed-off-by: francesco-racciatti <francesco.racciatti@sysdig.com>

--- roles.tf
+++ roles.tf
@@ -1,7 +1,35 @@
+locals {
+  secret_reference = local.do_fetch_secret ? split(":", var.access_key) : []
+}
+
 resource "aws_iam_role" "orchestrator_agent_execution_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
+
+  dynamic "inline_policy" {
+    for_each = local.do_fetch_secret ? ["SecretsManagerAccessKey"] : []
+    content {
+      name = "root"
+      policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+          {
+            Action = [
+              "secretsmanager:GetSecretValue",
+            ]
+            Effect = "Allow"
+            Resource = [format("arn:aws:secretsmanager:%s:%s:secret:%s",
+              element(local.secret_reference, 3),
+              element(local.secret_reference, 4),
+              element(local.secret_reference, 6)
+              )
+            ]
+          },
+        ]
+      })
+    }
+  }
 
   tags = merge(var.tags, var.default_tags)
 }
