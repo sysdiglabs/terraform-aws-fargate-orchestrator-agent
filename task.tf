4d232537d9824edffa142b9b19796e70b86a0bf8
francesco-racciatti <francesco.racciatti@sysdig.com>
2022-12-19 10:29:04 +0100

    refactor: fmt codebase
    Signed-off-by: Francesco Racciatti <francesco.racciatti@sysdig.com>

--- task.tf
+++ task.tf
@@ -1,22 +1,22 @@
 resource "aws_ecs_task_definition" "orchestrator_agent" {
-  family = "${var.name}-orchestrator-agent"
-  task_role_arn = "${aws_iam_role.orchestrator_agent_task_role.arn}"
-  execution_role_arn = "${aws_iam_role.orchestrator_agent_execution_role.arn}"
-  network_mode = "awsvpc"
+  family                   = "${var.name}-orchestrator-agent"
+  task_role_arn            = aws_iam_role.orchestrator_agent_task_role.arn
+  execution_role_arn       = aws_iam_role.orchestrator_agent_execution_role.arn
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu = "2048"
-  memory = "8192"
+  cpu                      = "2048"
+  memory                   = "8192"
 
   container_definitions = templatefile("${path.module}/container-definitions/orchestrator-agent.json", {
-    agent_image = var.agent_image
-    access_key = var.access_key
-    collector_host = var.collector_host
-    collector_port = var.collector_port
-    agent_tags = var.agent_tags
+    agent_image       = var.agent_image
+    access_key        = var.access_key
+    collector_host    = var.collector_host
+    collector_port    = var.collector_port
+    agent_tags        = var.agent_tags
     check_certificate = var.check_collector_certificate
     orchestrator_port = var.orchestrator_port
-    awslogs_region = data.aws_region.current_region.name
-    awslogs_group = "${var.name}-logs"
+    awslogs_region    = data.aws_region.current_region.name
+    awslogs_group     = "${var.name}-logs"
   })
 
   tags = merge(var.tags, var.default_tags)
