614f3a0eec46eb4170b86cce79368da4bb48eb0f
francesco-racciatti <francesco.racciatti@sysdig.com>
2023-02-04 15:04:07 +0100

    fix: update provider and remove wrong and unuseful api key
    Signed-off-by: francesco-racciatti <francesco.racciatti@sysdig.com>

--- example/provider.tf
+++ example/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     sysdig = {
       source  = "sysdiglabs/sysdig"
-      version = ">= 0.4.0"
+      version = ">= 0.5.47"
     }
   }
 }
@@ -10,8 +10,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-provider "sysdig" {
-  sysdig_secure_api_token = var.sysdig_access_key
-}
-
