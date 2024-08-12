16499cbe7ef797e4e8748ece627b0f3ff7a6fb24
Paul Crooks <paul.crooks@sysdig.com>
2024-08-12 13:27:32 +0000

    feat(logging): Add log retention configuration
    Ref: #24

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_ecs_cluster" "orchestrator_agent" {
 
 resource "aws_cloudwatch_log_group" "orchestrator_agent" {
   name = "${var.name}-logs"
+  retention_in_days = var.log_retention_days
 
   tags = merge(var.tags, var.default_tags)
 }
