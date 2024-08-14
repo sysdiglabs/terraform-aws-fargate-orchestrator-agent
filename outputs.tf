29e0242aef32a0e5c9872bb77820a046c5605238
Paul Crooks <paul.crooks@sysdig.com>
2024-08-14 08:15:31 +0000

    feat(output): Export Security Group ID

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "orchestrator_port" {
   description = "The configured port on the orchestrator"
   value       = var.orchestrator_port
 }
+
+output "orchestrator_security_group_id" {
+  description = "The security group ID of the orchestrator LB."
+  value       = aws_security_group.orchestrator_agent.id
+}
